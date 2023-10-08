4c262fb268dc7cfe2d9d1fae1e036dca8e0c1447
Mobina <mobina.mvd22@gmail.com>
2023-10-08 14:58:30 +0330

    LoginScreen is Done

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/widgets/curve_clipper.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final deviceHeight = MediaQuery.sizeOf(context).height;
+    final ThemeData themeData = Theme.of(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: SizedBox(
+          height: deviceHeight,
+          child: Column(
+            children: [
+              ClipPath(
+                clipper: CurveClipper(),
+                child: Image.asset(
+                  'assets/images/login_background.jpg',
+                  height: deviceHeight / 2.5,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Text(
+                'frenzy'.toUpperCase(),
+                style: TextStyle(
+                  color: themeData.colorScheme.primary,
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 10,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 15),
+                    fillColor: themeData.colorScheme.onPrimary,
+                    filled: true,
+                    hintText: 'Username',
+                    prefixIcon: const Icon(
+                      Icons.account_box,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(vertical: 15),
+                    fillColor: themeData.colorScheme.onPrimary,
+                    filled: true,
+                    hintText: 'Password',
+                    prefixIcon: const Icon(
+                      Icons.lock,
+                      size: 30,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 60),
+                  height: 45,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: themeData.colorScheme.primary,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: themeData.colorScheme.onPrimary,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: themeData.colorScheme.primary,
+                      height: 80,
+                      child: Text(
+                        'Don\'t have an account? Sign Up',
+                        style: TextStyle(
+                          color: themeData.colorScheme.onPrimary,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
